73de6ca9e41ffd3443c82e1b388e4076b9caf3a5
itizawa <itizawa.pen@gmail.com>
2025-02-11 14:33:20 +0900

    Change: コンテキストに session を載せた

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,18 +1,20 @@
-import { initTRPC } from "@trpc/server";
+import { auth } from "@/auth";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { cache } from "react";
 import superjson from "superjson";
 
 export const createTRPCContext = cache(async () => {
-  /**
-   * @see: https://trpc.io/docs/server/context
-   */
-  return { userId: "user_123" };
+  const session = await auth();
+
+  return {
+    currentUser: session?.user || null,
+  };
 });
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<typeof createTRPCContext>().create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
@@ -22,3 +24,13 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+  if (!ctx.currentUser) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({
+    ctx,
+  });
+});
