3555c0d2e89fe3fc76ff1b5363b5184241a2d2ff
itizawa <itizawa.pen@gmail.com>
2025-02-26 07:53:56 +0900

    Change: getCurrentUser ではそのままログイン中のユーザーを返す

--- src/trpc/routers/userRouter.ts
+++ src/trpc/routers/userRouter.ts
@@ -1,20 +1,7 @@
-import { prisma } from "@/prisma";
 import { baseProcedure, router } from "../init";
 
 export const userRouter = router({
   getCurrentUser: baseProcedure.query(async ({ ctx }) => {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: ctx.currentUser?.id,
-      },
-      include: {
-        planSubscription: {
-          select: {
-            plan: true,
-          },
-        },
-      },
-    });
-    return user;
+    return ctx.currentUser;
   }),
 });
