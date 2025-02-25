2058e7f80afc03f963894bfa3355bc8229d716c6
itizawa <itizawa.pen@gmail.com>
2025-02-26 07:34:09 +0900

    Change: seed にデータを追加

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.user.createMany({
+    data: [
+      { id: "agreement", email: "agreement@example.com", name: "賞賛BOT" },
+      { id: "feedback", email: "feedback@example.com", name: "指摘BOT" },
+      { id: "survey", email: "survey@example.com", name: "調査BOT" },
+    ],
+  });
+
   await prisma.plan.createMany({
     data: [
       { id: "ea4d1b33-371c-4358-84f1-3734ff9249c6", name: "admin" },
