5ca4cfffb194e98bd1ed12b555dd2f194e7fbc86
itizawa <itizawa.pen@gmail.com>
2025-03-01 21:36:16 +0900

    Change: サーバーサイドのデータを再取得する

--- src/app/actions/userActions.ts
+++ src/app/actions/userActions.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import { urls } from "@/consts/urls";
 import { AppRouter } from "@/trpc/routers/_app";
 import { trpc } from "@/trpc/server";
 import { inferRouterInputs } from "@trpc/server";
+import { revalidatePath } from "next/cache";
 
 export const getCurrentUser = async () => {
   return await trpc.user.getCurrentUser();
@@ -11,7 +13,12 @@ export const getCurrentUser = async () => {
 export const updateUserName = async ({
   name,
 }: inferRouterInputs<AppRouter>["user"]["updateUserName"]) => {
-  return await trpc.user.updateUserName({
+  const result = await trpc.user.updateUserName({
     name,
   });
+
+  // ダッシュボード関連のパスを再検証して、getCurrentUserのデータを再取得
+  revalidatePath(urls.dashboard);
+
+  return result;
 };
