33ddbc9f9fc093b3d7650bb974fb59d139edeb6b
itizawa <itizawa.pen@gmail.com>
2025-03-02 08:53:06 +0900

    Change: マイグレーションを実行して公開状態を表現した

--- src/types/src/domains/modelSchema/ThreadSchema.ts
+++ src/types/src/domains/modelSchema/ThreadSchema.ts
@@ -8,6 +8,7 @@ export const ThreadSchema = z.object({
   id: z.string().uuid(),
   userId: z.string(),
   title: z.string().nullable(),
+  isPublic: z.boolean(),
   createdAt: z.coerce.date(),
   updatedAt: z.coerce.date(),
 })
