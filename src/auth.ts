9bdccdd61c845708f73a46c6132f6e5d7f53a62c
itizawa <itizawa.pen@gmail.com>
2025-03-07 09:28:17 +0900

    Change: redirect

--- src/auth.ts
+++ src/auth.ts
@@ -2,9 +2,14 @@ import { prisma } from "@/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { urls } from "./consts/urls";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
   secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: urls.dashboard,
+    signOut: urls.top,
+  },
 });
