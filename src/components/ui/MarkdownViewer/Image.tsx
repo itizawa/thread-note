3610f491ed6739bd6ec2e388286eedf4cfde04ba
itizawa <itizawa.pen@gmail.com>
2025-03-16 10:02:47 +0900

    Change: 拡大する動作はなくした

--- src/components/ui/MarkdownViewer/Image.tsx
+++ src/components/ui/MarkdownViewer/Image.tsx
@@ -15,7 +15,7 @@ export const Image = ({ src, alt }: Props) => {
         src={src}
         alt={alt}
         className={
-          "max-w-full max-h-100 rounded-lg shadow-md mx-auto border cursor-zoom-in transition-transform transform hover:scale-105"
+          "max-w-full max-h-100 rounded-lg shadow-md mx-auto border cursor-zoom-in transition-transform transform"
         }
         onClick={() => setExpand(true)}
       />
