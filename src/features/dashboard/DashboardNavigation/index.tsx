c8c66e535421271a03a02ce966d9461e7daa7084
itizawa <itizawa.pen@gmail.com>
2025-03-22 11:40:02 +0900

    Clean: delete

--- src/features/dashboard/DashboardNavigation/index.tsx
+++ src/features/dashboard/DashboardNavigation/index.tsx
@@ -19,12 +19,6 @@ export const DashboardNavigation = () => {
             <h1 className="text-lg font-medium">Thread Note (β)</h1>
           </Link>
         </div>
-        {/* <div className="flex items-center space-x-2">
-          <Button variant="ghost" size="icon">
-            <Search className="h-5 w-5" />
-            <span className="sr-only">検索</span>
-            </Button>
-            </div> */}
         <Suspense fallback={<Skeleton className="w-8 h-8 rounded-full" />}>
           <NavigationUserIcon />
         </Suspense>
