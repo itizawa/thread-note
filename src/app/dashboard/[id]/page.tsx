703aa6bc8187a9b22317b5b0d8b8cf26afc1dbef
itizawa <itizawa.pen@gmail.com>
2025-04-01 23:32:17 +0900

    Change: OGPをデフォルトのデータを返すようにする

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -2,7 +2,6 @@ import { getCurrentUser } from "@/app/actions/userActions";
 import { PostTimeLine } from "@/features/threadDetail/PostTimeLine";
 import { urls } from "@/shared/consts/urls";
 import { generateMetadataObject } from "@/shared/lib/generateMetadataObject";
-import { trpc } from "@/trpc/server";
 import { Metadata, NextSegmentPage } from "next";
 import { redirect } from "next/navigation";
 
@@ -15,20 +14,8 @@ export const generateMetadata = async ({
 }>["arguments"]): Promise<Metadata> => {
   const { id: threadId } = await params;
 
-  const thread = await trpc.thread.getThreadInfo({
-    id: threadId,
-  });
-
-  if (!thread)
-    return generateMetadataObject({
-      title: "Thread Not Found",
-      description: "The requested thread does not exist.",
-      images: ["/api/og?title=NotFound"],
-    });
-
   return generateMetadataObject({
-    title: thread.title || undefined,
-    images: [`/api/og?title=${encodeURIComponent(thread.title || "")}`],
+    title: `${threadId}の編集ページ`,
   });
 };
 
