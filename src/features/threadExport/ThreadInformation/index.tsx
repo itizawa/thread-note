531ee97811698bec8a0cf94285db252253b1417a
itizawa <itizawa.pen@gmail.com>
2025-04-01 23:36:06 +0900

    Change: 読み込み中

--- src/features/threadExport/ThreadInformation/index.tsx
+++ src/features/threadExport/ThreadInformation/index.tsx
@@ -6,8 +6,11 @@ import { LinkToBack } from "@/shared/ui/LinkToBack";
 import { Tooltip } from "@/shared/ui/Tooltip";
 import { trpc } from "@/trpc/client";
 import { RefreshCw } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
 export function ThreadInformation({ threadId }: { threadId: string }) {
+  const [isPending, startTransition] = useTransition();
   const [threadInfo, { refetch: refetchThreadInfo }] =
     trpc.thread.getThreadInfo.useSuspenseQuery({
       id: threadId,
@@ -19,8 +22,12 @@ export function ThreadInformation({ threadId }: { threadId: string }) {
     });
 
   const handleClickRefetch = () => {
-    refetchThreadInfo();
-    refetchPosts();
+    startTransition(async () => {
+      await refetchThreadInfo();
+      await refetchPosts();
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      toast.success("更新しました");
+    });
   };
 
   if (!threadInfo) {
@@ -50,8 +57,10 @@ export function ThreadInformation({ threadId }: { threadId: string }) {
             size="icon"
             className="transition-opacity hover:bg-gray-200"
             onClick={handleClickRefetch}
+            loading={isPending}
+            disabled={isPending}
           >
-            <RefreshCw className="h-4 w-4" />
+            {isPending ? null : <RefreshCw className="h-4 w-4" />}
           </Button>
         </Tooltip>
       </div>
