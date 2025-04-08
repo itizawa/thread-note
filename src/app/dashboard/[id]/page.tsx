f05c1bd0b423c95169591bdc1241151ae1d55db6
itizawa <itizawa.pen@gmail.com>
2025-04-08 09:03:50 +0900

    Fix: ogp

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getCurrentUser } from "@/app/actions/userActions";
 import { PostTimeLine } from "@/features/threadDetail/PostTimeLine";
 import { urls } from "@/shared/consts/urls";
 import { generateMetadataObject } from "@/shared/lib/generateMetadataObject";
+import { trpc } from "@/trpc/server";
 import { Metadata, NextSegmentPage } from "next";
 import { redirect } from "next/navigation";
 
@@ -13,9 +14,20 @@ export const generateMetadata = async ({
   params: { id: string };
 }>["arguments"]): Promise<Metadata> => {
   const { id: threadId } = await params;
+  const thread = await trpc.thread.getThreadInfo({
+    id: threadId,
+  });
+
+  if (!thread)
+    return generateMetadataObject({
+      title: "Thread Not Found",
+      description: "The requested thread does not exist.",
+      images: ["/api/og?title=NotFound"],
+    });
 
   return generateMetadataObject({
-    title: `${threadId}の編集ページ`,
+    title: thread.title || undefined,
+    images: [`/api/og?title=${encodeURIComponent(thread.title || "")}`],
   });
 };
 
