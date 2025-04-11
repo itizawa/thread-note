bd5a57f24c4c3aac82ed180bceeee4e64b3928d7
itizawa <itizawa.pen@gmail.com>
2025-04-11 10:10:54 +0900

    Change: input で error メッセージを表示できるようにした

--- src/shared/ui/input.tsx
+++ src/shared/ui/input.tsx
@@ -4,8 +4,12 @@ import { cn } from "@/shared/lib/utils";
 
 const Input = React.forwardRef<
   HTMLInputElement,
-  React.ComponentProps<"input"> & { forceFocus?: boolean }
->(({ className, type, forceFocus, ...props }, ref) => {
+  React.ComponentProps<"input"> & {
+    forceFocus?: boolean;
+    error?: boolean;
+    errorMessage?: string;
+  }
+>(({ className, type, forceFocus, error, errorMessage, ...props }, ref) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
 
   React.useEffect(() => {
@@ -18,15 +22,21 @@ const Input = React.forwardRef<
   }, [forceFocus]);
 
   return (
-    <input
-      type={type}
-      className={cn(
-        "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
-      )}
-      ref={ref || inputRef}
-      {...props}
-    />
+    <div className="flex flex-col space-y-2">
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          error
+            ? "border-red-500 focus-visible:ring-red-500"
+            : "border-input focus-visible:ring-ring",
+          className
+        )}
+        ref={ref || inputRef}
+        {...props}
+      />
+      {errorMessage && <p className="text-red-500 text-xs">{errorMessage}</p>}
+    </div>
   );
 });
 Input.displayName = "Input";
