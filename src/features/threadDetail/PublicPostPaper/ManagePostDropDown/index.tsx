4cd6b800d8d841cb313e777cd1c6ea58f793d348
itizawa <itizawa.pen@gmail.com>
2025-10-19 21:00:53 +0900

    fix

--- src/features/threadDetail/PublicPostPaper/ManagePostDropDown/index.tsx
+++ src/features/threadDetail/PublicPostPaper/ManagePostDropDown/index.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@/shared/ui/button";
+import { IconButton } from "@/shared/components/IconButton";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/shared/ui/dropdown-menu";
 import { DropdownMenuItemWithIcon } from "@/shared/ui/dropdown-menu-item-with-icon";
+import { MoreHorizOutlined } from "@mui/icons-material";
 
-import { Link, MoreHorizontal } from "lucide-react";
+import { Link } from "lucide-react";
 
 type Props = {
   onClickShareButton: () => void;
@@ -16,10 +17,9 @@ export function ManagePostDropDown({ onClickShareButton }: Props) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <MoreHorizontal className="h-4 w-4" />
-          <span className="sr-only">メニューを開く</span>
-        </Button>
+        <IconButton size="small">
+          <MoreHorizOutlined />
+        </IconButton>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItemWithIcon
