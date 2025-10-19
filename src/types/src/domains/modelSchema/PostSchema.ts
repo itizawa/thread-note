c274a1be8d561d4849b55021703d6a084bad6a7d
itizawa <itizawa.pen@gmail.com>
2025-10-19 21:51:03 +0900

    fix ts err

--- src/types/src/domains/modelSchema/PostSchema.ts
+++ src/types/src/domains/modelSchema/PostSchema.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 /////////////////////////////////////////
 // POST SCHEMA
@@ -6,15 +6,15 @@ import { z } from 'zod';
 
 export const PostSchema = z.object({
   id: z.string().uuid(),
-  body: z.string({ required_error: '入力してください' }),
+  body: z.string({ message: "入力してください" }),
   userId: z.string(),
   createdAt: z.coerce.date(),
   updatedAt: z.coerce.date(),
   threadId: z.string(),
   parentId: z.string().nullable(),
   isArchived: z.boolean(),
-})
+});
 
-export type Post = z.infer<typeof PostSchema>
+export type Post = z.infer<typeof PostSchema>;
 
 export default PostSchema;
